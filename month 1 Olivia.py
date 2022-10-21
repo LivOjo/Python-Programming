8008e6547afed247c42f14b578972dc1303564bb
LivOjo <116380989+LivOjo@users.noreply.github.com>
2022-10-21 19:12:16 +0100

    Add files via upload

---
+++ month 1 Olivia.py
@@ -0,0 +1,53 @@
+'''
+practical assessment
+'''
+
+#Create 5 variables
+pupil='Denzel'
+pupil2=1
+adult=2.5
+adult2=False
+twins=2
+
+
+#Output
+print(pupil)
+print(pupil2)
+print(adult)
+print(adult2)
+print(twins)
+
+#Types
+print(type(pupil))
+print(type(pupil2))
+print(type(adult))
+print(type(adult2))
+print(type(twins))
+
+#creating 2 variables
+student='Denzel'
+student2='Olivia'
+
+#methods
+print(student.upper())
+print(student2.lower())
+print(student.title())
+
+#arithmetic operations
+print(2*4)
+print(1 + 1)
+print(3-3)
+print(7*7)
+print(10/3)
+print(9//3)
+print(5%3)
+print(5**3)
+
+
+#methods and placeholder
+first_name ='olivia'
+last_name ='ojo'
+full_name =f'{first_name} {last_name}'
+message =f'hello {full_name.title()}'
+print(message)
+
